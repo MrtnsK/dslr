f641adb9c59e60e5926a77a58a81d6b762f587b0
Kevin <kemartin@student.42.fr>
2020-09-11 23:04:37 +0200

    use sample standard deviation equation for std value

--- describe.py
+++ describe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 import sys
+from math import sqrt
 
 def describe(dataset):
 	data = dataset[dataset.columns[6:19]]
@@ -14,11 +15,18 @@ def describe(dataset):
 	vmax = []
 	for i in range(data.shape[1]):
 		c = data.iloc[:,i].tolist()
+		c = [x for x in c if str(x) != 'nan']
 		c.sort
 		count.append(len(c))
 		mean.append(sum(c) / len(c))
-		vmin = min(c)
-		vmax = max(c)
+		vmin.append(min(c))
+		vmax.append(max(c))
+		result = 0
+		for i in range(len(c)):
+			result += (c[i] - sum(c) / len(c))**2 
+		result = result / (len(c) - 1)
+		std = sqrt(result)
+		print (std)
 
 if __name__ == "__main__":
 	if (len(sys.argv) < 2):
