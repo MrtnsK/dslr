a57ac0a2616f9ff407eb56673045198e7b970742
Kevin MARTINS <kemartin@e1r7p8.42.fr>
2020-09-17 16:21:42 +0200

    i'm kinda lost with that cost thing

--- logreg_train.py
+++ logreg_train.py
@@ -8,18 +8,27 @@ def StandardScaler(X):
     scale = np.std(X - mean, axis=0)
     return (X - mean) / scale
 
-def OfA(X, y):
+def	cost(X, y, theta, passes):
 	
+
+def OfA(X, y):
+	theta = 0
+	m = float(len(X))
+	for passes in range(2000):
+		theta = theta - 1 * (1 / m) * (X.T @ ((X @ theta) - y))
+		cost(X, y, theta, passes)
 
 def	LogisticRegression(X, y):
 	y_g = y.replace({"Gryffindor" : 1 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 0})
 	y_s = y.replace({"Gryffindor" : 0 , "Slytherin" : 1, "Ravenclaw" : 0, "Hufflepuf" : 0})
 	y_r = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 1, "Hufflepuf" : 0})
 	y_h = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 1})
-	thetas_g = OfA()
-	thetas_s = OfA()
-	thetas_r = OfA()
-	thetas_h = OfA()
+	theta_g = OfA(X, y_g)
+	theta_s = OfA(X, y_s)
+	theta_r = OfA(X, y_r)
+	theta_h = OfA(X, y_h)
+	thetas = [theta_g, theta_s, theta_r, theta_h]
+	return thetas
 
 if __name__ == "__main__":
 	if (len(sys.argv) < 2):
@@ -32,5 +41,6 @@ if __name__ == "__main__":
 	dataset = dataset.drop('Defense Against the Dark Arts', axis=1)
 	X = np.array(dataset)
 	print(X)
-	X = StandardScaler(X)               
+	X = StandardScaler(X)
+	X = np.c_[np.ones(X.shape[0]), X]
 	print(X)
