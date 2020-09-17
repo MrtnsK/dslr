d95b140be9f58c5353bbf13ab94260e1d8f91d23
Kevin MARTINS <kemartin@e1r7p8.42.fr>
2020-09-17 18:29:56 +0200

    lost lost

--- logreg_train.py
+++ logreg_train.py
@@ -8,26 +8,29 @@ def StandardScaler(X):
     scale = np.std(X - mean, axis=0)
     return (X - mean) / scale
 
-def	cost(X, y, theta, passes):
+# def	cost(X, y, theta, passes):
 	
 
-def OfA(X, y):
-	theta = 0
-	m = float(len(X))
-	for passes in range(2000):
-		theta = theta - 1 * (1 / m) * (X.T @ ((X @ theta) - y))
-		cost(X, y, theta, passes)
+# def OfA(X, y):
+# 	theta = 0
+# 	m = float(len(X))
+# 	for passes in range(2000):
+# 		theta = theta - 1 * (1 / m) * (X.T @ ((X @ theta) - y))
+		# cost(X, y, theta, passes)
+
+def Xavier(X):
+    return np.random.randn(X.shape[1]) * np.sqrt(1 / X.shape[1])
 
 def	LogisticRegression(X, y):
-	y_g = y.replace({"Gryffindor" : 1 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 0})
-	y_s = y.replace({"Gryffindor" : 0 , "Slytherin" : 1, "Ravenclaw" : 0, "Hufflepuf" : 0})
-	y_r = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 1, "Hufflepuf" : 0})
-	y_h = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 1})
-	theta_g = OfA(X, y_g)
-	theta_s = OfA(X, y_s)
-	theta_r = OfA(X, y_r)
-	theta_h = OfA(X, y_h)
-	thetas = [theta_g, theta_s, theta_r, theta_h]
+	# y_g = y.replace({"Gryffindor" : 1 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 0})
+	# y_s = y.replace({"Gryffindor" : 0 , "Slytherin" : 1, "Ravenclaw" : 0, "Hufflepuf" : 0})
+	# y_r = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 1, "Hufflepuf" : 0})
+	# y_h = y.replace({"Gryffindor" : 0 , "Slytherin" : 0, "Ravenclaw" : 0, "Hufflepuf" : 1})
+	# theta_g = OfA(X, y_g)
+	# theta_s = OfA(X, y_s)
+	# theta_r = OfA(X, y_r)
+	# theta_h = OfA(X, y_h)
+	# thetas = [theta_g, theta_s, theta_r, theta_h]
 	return thetas
 
 if __name__ == "__main__":
@@ -40,7 +43,6 @@ if __name__ == "__main__":
 	dataset = dataset.drop('Astronomy', axis=1)
 	dataset = dataset.drop('Defense Against the Dark Arts', axis=1)
 	X = np.array(dataset)
-	print(X)
 	X = StandardScaler(X)
 	X = np.c_[np.ones(X.shape[0]), X]
-	print(X)
+	print(Xavier(X))
